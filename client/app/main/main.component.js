1bf36f99556ab3fcb9b31541038f76d5caeaea66
Mandi Grant <magrant@expedia.com>
2017-07-01 13:08:24 -0700

    Working on building the basic data structure for all the courses. Right now it gets the class name, truncates it down to something like CS161, and builds an object with that as its key. The value will be an object containing the full name, an array for reviews, an array for comments, etc.

--- client/app/main/main.component.js
+++ client/app/main/main.component.js
@@ -6,13 +6,27 @@ var jsonToPivotjson = require("json-to-pivot-json");
 export class MainController {
 
   awesomeThings = [];
+  courseData = {};
+
   newThing = '';
   classNames = [];
 
   /*@ngInject*/
   constructor($http) {
     this.$http = $http;
+    this.underscore = require('underscore');
   }
+
+  /*
+  courses: {
+    CS161: {
+      fullName: "CS161 Intro to Computer Science",
+      reviews: ["sdasda", "wow such hard", "easy"]
+    },
+    CS162: {
+      ...
+    }
+   */
 
   $onInit() {
     this.$http.get('/api/things')
@@ -20,7 +34,23 @@ export class MainController {
         //all class data
         //todo: refactor boilerplate naming
         this.awesomeThings = response.data;
-        //just the class names
+        console.log(this.awesomeThings);
+        //first group all the data by course name
+        this.courses = this.underscore.chain(this.awesomeThings)
+          .groupBy('whatCourseDidYouTake?')
+          .value();
+
+        console.log("courses grouped by name:", this.courses);
+
+        //get the "CS161" name from each course and build objects out of these
+        this.underscore.each(this.courses, ((courses) => {
+          this.underscore.each(courses, ((course) => {
+            let str = course['whatCourseDidYouTake?'].substring(0,6).split(' ').join('');
+              this.courseData[str] = {'fullName':course['whatCourseDidYouTake?']}
+          }))
+        }));
+        console.log(this.courseData);
+
         var options = {
           row: "whatCourseDidYouTake?"
         };
