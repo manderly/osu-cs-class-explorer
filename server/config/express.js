f7ca3054fc79cb3b7a7129118c87d1ceb5320be3
Mandi <mandi018@gmail.com>
2021-05-05 18:30:15 -0500

    Updated node version, removed broken package that wasn't used anyway, changed my name, changed github link, added Google adsense code

--- server/config/express.js
+++ server/config/express.js
@@ -7,7 +7,6 @@
 import express from 'express';
 import favicon from 'serve-favicon';
 import morgan from 'morgan';
-import shrinkRay from 'shrink-ray';
 import bodyParser from 'body-parser';
 import methodOverride from 'method-override';
 import cookieParser from 'cookie-parser';
@@ -38,7 +37,6 @@ export default function(app) {
   app.set('views', `${config.root}/server/views`);
   app.engine('html', require('ejs').renderFile);
   app.set('view engine', 'html');
-  app.use(shrinkRay());
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(bodyParser.json());
   app.use(methodOverride());
